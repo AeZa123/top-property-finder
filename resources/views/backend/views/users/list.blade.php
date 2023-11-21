3849741ac9886f9eb0b91bb25beb817a3f3d7b68
Ae <aymexe1212@gmail.com>
2023-11-21 19:49:06 +0700

    update

--- resources/views/backend/views/users/list.blade.php
+++ resources/views/backend/views/users/list.blade.php
@@ -7,7 +7,118 @@
 
             <!-- Main row -->
             <div class="row">
+
+
                 <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Manage Users</h3>
+                            <a href="{{ route('user.create') }}" class="btn btn-success btn-sm float-right">Create user</a>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <div class="search-box float-right" style="margin-top: -3px;">
+                                {{-- <form action="#">
+                                    <input style="width: 350px; height: 35px;" class="form-control" type="text"
+                                        id="searcaah" name="searcaah" placeholder="Search..." required>
+                                    <i class="ti-search"></i>
+                                </form> --}}
+                            </div>
+                            <table id="example1" class="table table-hover table-bordered table-striped">
+                                <thead class="text-white text-center" style="background-color: #00B98E;">
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>ชื่อ-นามสกุล</th>
+                                        <th>อีเมล</th>
+                                        <th>เบอร์โทร</th>
+                                        <th>status</th>
+                                        <th>created at</th>
+                                        <th>action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                    @foreach ($users as $user)
+                                        <tr class="text-center">
+                                            <td>{{ $user->id }}</td>
+                                            <td>
+                                                <a href="#">
+                                                    <div class="user-panel d-flex align-items-center">
+                                                        @if ($user->avatar != null)
+                                                            <div>
+                                                                <img src="{{ asset('storage/images/users/' . $user->avatar) }}"
+                                                                    class="img-circle " alt="User Image">
+                                                            </div>
+                                                        @else
+                                                            <div>
+                                                                <img src="{{ asset('storage/images/users/653d4e8e1a4e0.png') }}"
+                                                                    class="img-circle " alt="User Image">
+                                                            </div>
+                                                        @endif
+                                                        <div class="ml-2">
+                                                            {{ $user->fname }} {{ $user->lname }}
+                                                        </div>
+                                                    </div>
+                                                </a>
+                                            </td>
+                                            <td>{{ $user->email }}</td>
+                                            <td>{{ $user->tel }}</td>
+                                            <td>
+                                                <?php
+                                                if ($user->role == 1) {
+                                                    $code_color_text = '#e11d48';
+                                                    $code_color_bg = '#ffe4e6';
+                                                } elseif ($user->role == 2) {
+                                                    $code_color_text = '#ea580c';
+                                                    $code_color_bg = '#ffedd5';
+                                                } elseif ($user->role == 3) {
+                                                    $code_color_text = '#0891b2';
+                                                    $code_color_bg = '#cffafe';
+                                                }
+                                                
+                                                ?>
+
+                                                <div class="row justify-content-center">
+                                                    <div class="col-md-12 text-center">
+                                                        <div class=""
+                                                            style="border-radius: 12px; background-color: {{ $code_color_bg }}; color:{{ $code_color_text }};">
+                                                            <b>{{ $user->role_name }}</b>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                            </td>
+                                            <td>{{ \Carbon\Carbon::parse($user->created_at)->format('d/m/Y') }}</td>
+
+                                            <td>
+                                                <a href="{{ url('user/edit/' . $user->id) }}">
+                                                    <i class="fas fa-edit btn btn-warning"></i>
+                                                </a>
+                                                <a href="#" data-id="{{ $user->id }}" id="deleteBtn">
+                                                    <i class="fas fa-trash-alt btn btn-danger text-white"></i>
+                                                </a>
+                                            </td>
+                                           
+
+                                        </tr>
+                                    @endforeach
+
+
+                                </tbody>
+
+                            </table>
+
+                            <div style="float: right">
+                                {{ $users->links() }}
+                            </div>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                </div>
+
+
+
+                {{-- <div class="col-md-12">
                     <div class="card">
 
                         <div class="card-header">
@@ -44,34 +155,6 @@
                                             @foreach ($users as $user)
                                                 <tr>
                                                     <th scope="row">{{ $user->id }}</th>
-                                                    {{-- <td><a href="{{url('showBlog/'.$user->id)}}" target="_bank">Test </a></td> --}}
-                                                    {{-- <td>
-                                                        <div class="row justify-content-center align-items-center">
-                                                            
-                                                            <div class="col-md-3 text-right">
-                                                                <div class="user-panel">
-                                                                    @if ($user->avatar != null)
-                                                                        <div>
-                                                                            <img src="{{ asset('storage/images/users/' . $user->avatar) }}"
-                                                                                class="img-circle " alt="User Image">
-                                                                        </div>
-                                                                    @else
-                                                                        <div>
-                                                                            <img src="{{ asset('storage/images/users/' . $user->avatar) }}"
-                                                                                class="img-circle " alt="User Image">
-                                                                        </div>
-                                                                    @endif
-                                                                </div>
-                                                            </div>
-                                                            <div class="col-md-9 text-left">
-                                                                <div class="ml-2">
-                                                                    {{ $user->fname }} {{ $user->lname }}
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                      
-                                                    </td> --}}
-
                                                     <td>
                                                         <a href="#">
 
@@ -126,10 +209,6 @@
                                                     <td>{{ \Carbon\Carbon::parse($user->created_at)->format('d/m/Y') }}
                                                     </td>
                                                     <td>
-                                                        {{-- <a href="{{url('blog/laravel/edit/'.$blog->id)}}">
-                                                            <i class="ti-pencil-alt pr-3 text-warning" title="Edit"></i>
-                                                        </a> --}}
-
                                                         <a href="{{ url('user/edit/' . $user->id) }}">
                                                             <i class="fas fa-edit btn btn-warning"></i>
                                                         </a>
@@ -140,10 +219,7 @@
                                                     </td>
                                                 </tr>
                                             @endforeach
-                                            {{-- <a class="display-6" href="{{url('showBlog/'.$blog->id)}}" target="_bank">{{$blog->title}}</a> --}}
-
-
-
+                                          
                                         </tbody>
                                     </table>
                                 </div>
@@ -155,13 +231,18 @@
                     </div>
 
 
-                </div>
+                </div> --}}
+
+
+
             </div>
         </div>
 
 
     </section>
     <!-- /.content -->
+
+
 
     <script type="text/javascript">
         var delayTimer;
